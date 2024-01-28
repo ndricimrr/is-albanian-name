350452ebec9efba73f2a20ce43721add3f1a52c4
ndricimrr <ndricimrr@gmail.com>
2024-01-28 17:58:58 +0100

    Add npm test step

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-// Import file from a npm package instead.
-// import ('albanian-names-dataset')
-// this should make it easier to maintain this package.
-// Code on this file should be super plain simple. The whole idea of making it just a simple exportable Node module is so that it does not require lots of maintaince in the future.
-
 "use strict";
-
-const someNamesTest = [
-  {
-    name: "Adelina",
-    gender: "F",
-    origin: "",
-  },
-  {
-    name: "Bardhyl",
-    gender: "M",
-    origin: "",
-  },
-  {
-    name: "Elira",
-    gender: "F",
-    origin: "",
-  },
-  {
-    name: "Gentian",
-    gender: "M",
-    origin: "",
-  },
-];
-
-const someNamesTestList = ["Adelina", "Bardhyl", "Elira", "Gentian"];
 
 const all_names = require("albanian-names-dataset/all_names.json");
 
